6c420bbd05c5227cee20b03a33be23a69e1eef39
nayara <vitorianayy6@gmail.com>
2026-06-04 15:46:18 -0400

    Integração com a API

--- src/app/features/projects/pages/coordinator/coordinator.page.ts
+++ src/app/features/projects/pages/coordinator/coordinator.page.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 
 import { API_CONFIG } from '../../../../core/config/api.config';
@@ -10,11 +10,11 @@ import { API_CONFIG } from '../../../../core/config/api.config';
   styleUrls: ['./coordinator.page.scss'],
   standalone: false,
 })
-export class CoordinatorPage {
+export class CoordinatorPage implements OnInit {
   isLoading = false;
   errorMessage = '';
   responseContent = '';
-  token = 'seu_token_aqui';
+  token = 'eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJxLXByb2pldG9zLWFwaSIsInN1YiI6ImNvb3JkZW5hZG9yQGlmcGUuZWR1LmJyIiwicm9sZSI6IlJPTEVfQ09PUkQiLCJleHAiOjE3ODA2MTE4OTh9.ZtkhLkxlJyHFc4Qv8LjZRazzK5XUFhbGhgPTEnJSR6M';
 
   constructor(private readonly http: HttpClient) {}
 
@@ -24,15 +24,35 @@ export class CoordinatorPage {
   mostrarProjetosEncerrados = false;
 
   projetos: any[] = [];
-
   leads: any[] = [];
+  meusProjetos: any[] = [];
+
+  ngOnInit(): void {
+    this.loadCounters();
+  }
+
+  get qtdProjetosMeus(): number {
+    return this.meusProjetos.length;
+  }
+
+  get qtdProjetosAtivos(): number {
+    return this.projetos.filter((projeto) => projeto.status !== 'ENCERRADA').length;
+  }
+
+  get qtdProjetosEncerrados(): number {
+    return this.projetos.filter((projeto) => projeto.status === 'ENCERRADA').length;
+  }
+
+  get qtdInteressados(): number {
+    return this.leads.length;
+  }
 
   get projetosAtivos() {
-    return this.projetos.filter((projeto) => projeto.status !== 'Encerrado');
+    return this.projetos.filter((projeto) => projeto.status !== 'ENCERRADA');
   }
 
   get projetosEncerrados() {
-    return this.projetos.filter((projeto) => projeto.status === 'Encerrado');
+    return this.projetos.filter((projeto) => projeto.status === 'ENCERRADA');
   }
 
   toggleMeusProjetos(): void {
@@ -41,8 +61,6 @@ export class CoordinatorPage {
       this.mostrarProjetosAtivos = false;
       this.mostrarInteressados = false;
       this.mostrarProjetosEncerrados = false;
-      //this.loadProjects("/projetos/meus-projetos");
-      this.projetos = [];
     }
   }
 
@@ -52,7 +70,6 @@ export class CoordinatorPage {
       this.mostrarMeusProjetos = false;
       this.mostrarInteressados = false;
       this.mostrarProjetosEncerrados = false;
-      this.loadProjects("/projetos");
     }
   }
 
@@ -71,19 +88,48 @@ export class CoordinatorPage {
       this.mostrarMeusProjetos = false;
       this.mostrarProjetosAtivos = false;
       this.mostrarInteressados = false;
+      //this.loadProjects("/projetos/status?status=ENCERRADA");
     }
   }
 
-  async loadProjects(endpoint: string): Promise<void> {
+  async loadCounters(): Promise<void> {
     this.isLoading = true;
     this.errorMessage = '';
     this.responseContent = '';
 
-    this.projetos = await this.execGet(endpoint);
+    try {
+      const [todos, meus] = await Promise.all([
+        this.fetchProjects('/projetos'),
+        this.fetchProjects('/projetos/meus-projetos'),
+      ]);
+
+      this.projetos = todos;
+      this.meusProjetos = meus;
+      await this.loadLeaders();
+      
+    } catch (error: any) {
+      this.errorMessage = error?.message || 'Falha ao carregar contadores.';
+      this.responseContent = JSON.stringify(error?.error ?? error, null, 2);
+    } finally {
+      this.isLoading = false;
+    }
   }
+
+  async loadLeaders(): Promise<void> {
+    if(this.meusProjetos && this.meusProjetos.length > 0) {
+      const leaders: any[] = [];
+      this.meusProjetos.forEach(async (projeto) =>{
+        const leadersProjeto = await this.fetchLeaders(`/interesses/projeto/${projeto.id}`);
+        leaders.push(...leadersProjeto);
+      });
+      this.leads = leaders;
+    }
+  }
+
 
   private mapApiProjectsToView(projects: any[]): any[] {
     return projects.map((item) => ({
+      id: item.id ?? 'N/A',
       titulo: item.titulo ?? 'Sem título',
       categoria: item.tipo ?? item.modalidade ?? 'Não informado',
       vagas: item.vagas ?? 0,
@@ -95,27 +141,50 @@ export class CoordinatorPage {
     }));
   }
 
-  // param: endpoint - string endpoint da API (ex: '/projetos/ativos')
-  private async execGet(endpoint: string): Promise<any> {
-    try {
-      const url = `${API_CONFIG.baseUrl}${endpoint}`;
-      const headers = new HttpHeaders({
-        Authorization: `Bearer ${this.token}`,
-        'Content-Type': 'application/json',
-      });
+  // param: endpoint - string endpoint da API (ex: '/projetos/meus-projetos')
+  private async fetchProjects(endpoint: string): Promise<any[]> {
+    const url = `${API_CONFIG.baseUrl}${endpoint}`;
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`,
+      'Content-Type': 'application/json',
+    });
 
-      const response = await firstValueFrom(this.http.get(url, { headers }));
-      this.responseContent = JSON.stringify(response, null, 2);
-
-      if (Array.isArray(response)) {
-        return this.mapApiProjectsToView(response);
-      }
-    } catch (error: any) {
-      this.errorMessage = error?.message || 'Falha ao carregar projetos.';
-      this.responseContent = JSON.stringify(error?.error ?? error, null, 2);
-    } finally {
-      this.isLoading = false;
+    const response = await firstValueFrom(this.http.get(url, { headers }));
+  
+    if (Array.isArray(response)) {
+      return this.mapApiProjectsToView(response);
     }
+    return [];
   }
+
+  private mapApiLeadersToView(leaders: any[]): any[] {
+    return leaders.map((item) => ({
+      id: item.id ?? 'N/A',
+      idProjeto: item.idProjeto ?? 'N/A',
+      tituloProjeto: item.tituloProjeto ?? 'Sem título',
+      nome: item.nome ?? 'Sem nome',
+      email: item.email ?? 'Sem email',
+      seriePeriodo: item.seriePeriodo ?? 'Não informado',
+      modalidadePretendida: item.modalidadePretendida ?? 'Não informado',
+      aceitouLgpd: item.aceitouLgpd ? 'Sim' : 'Não',
+      dataRegistro: item.dataRegistro ?? 'Não informado',
+    }));
+  }
+
+  private async fetchLeaders(endpoint: string): Promise<any[]> {
+    const url = `${API_CONFIG.baseUrl}${endpoint}`;
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`,
+      'Content-Type': 'application/json',
+  });
+
+    const response = await firstValueFrom(this.http.get(url, { headers }));
+    console.log(response);
+    if (Array.isArray(response)) {
+      return this.mapApiLeadersToView(response);
+    }
+    return [];
+  }
+
 }
 
